05e0a4eb431b055da2d22b6bea40f1ce4807ee71
Anton Mashkov <rumpil666@gmail.com>
2023-05-23 19:14:04 +0400

    h

--- backend/middlewares/errorHandler.js
+++ backend/middlewares/errorHandler.js
@@ -1,8 +1,10 @@
+const httpConstants = require('http2').constants;
+
 const errorHandler = (err, req, res, next) => {
   const status = err.statusCode;
   console.log(err.stack || err);
   const { message } = err;
-  if (!err.statusCode) {
+  if (status === httpConstants.HTTP_STATUS_INTERNAL_SERVER_ERROR) {
     res.status(status).send({
       message: 'Server Error',
     });

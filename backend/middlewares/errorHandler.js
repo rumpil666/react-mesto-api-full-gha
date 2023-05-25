cd03bcc487738da19037e3e15407df0ecf226f61
Anton Mashkov <rumpil666@gmail.com>
2023-05-25 19:43:17 +0400

    error 500

--- backend/middlewares/errorHandler.js
+++ backend/middlewares/errorHandler.js
@@ -1,9 +1,11 @@
+const httpConstants = require('http2').constants;
+
 const errorHandler = (err, req, res, next) => {
   const status = err.statusCode;
   console.log(err.stack || err);
   const { message } = err;
   if (!status) {
-    res.status(status).send({
+    res.status(httpConstants.HTTP_STATUS_INTERNAL_SERVER_ERROR).send({
       message: 'Server Error',
     });
   } else {
